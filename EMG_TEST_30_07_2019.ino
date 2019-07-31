a0d5bf9e51c81e1791b35acc6865783eff84516b
Fabian Bustos <wfabianquijano@gmail.com>
2019-07-30 23:01:04 -0500

    Add files via upload

---
+++ EMG_TEST_30_07_2019.ino
@@ -0,0 +1,14 @@
+int v=A0;
+int valor;
+
+void setup() {
+  Serial.begin(250000);
+  
+}
+
+void loop() {
+  valor= map(analogRead(v),0,1023,10000,0);
+//  //valor=valor*0.004;
+  Serial.println(valor);
+  delay(10);
+}
